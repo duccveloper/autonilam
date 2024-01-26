45af12ef5914e3702f321b58146a9a332b13bf5a
du-cc <67620716+du-cc@users.noreply.github.com>
2024-01-26 18:34:42 +0800

    Update betterNilam.user.js

--- betterNilam.user.js
+++ betterNilam.user.js
@@ -1,1 +1,15 @@
-document.head.appendChild(Object.assign(document.createElement('script'), { type: 'text/javascript', src: 'https://cdn.jsdelivr.net/gh/du-cc/BetterNilam@main/source.js' }));
+// ==UserScript==
+// @name         betterNilam
+// @namespace    huh
+// @version      RELEASE
+// @description  idk
+// @author       du-cc
+// @match        https://nilamjohor.edu.my/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.my
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+  document.head.appendChild(Object.assign(document.createElement('script'), { type: 'text/javascript', src: 'https://cdn.jsdelivr.net/gh/du-cc/BetterNilam@main/source.js' }));
+})();
