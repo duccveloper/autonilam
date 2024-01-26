87d7e048147072b72a0f220a59d30f5ac361d2b0
du-cc <67620716+du-cc@users.noreply.github.com>
2024-01-26 14:53:15 +0800

    Create betterNilam.js

---
+++ betterNilam.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/du-cc/BetterNilam@main/source.js';
+document.head.appendChild(script);
